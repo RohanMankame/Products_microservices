abf4401645b5f37193848d14b9682790225d75f4
Rohan Mankame <rohanmankame@outlook.com>
2025-10-19 14:22:01 +0530

    first commit

---
+++ inventory/main.py
@@ -0,0 +1,48 @@
+from typing import Union
+
+from fastapi import FastAPI
+from redis_om import get_redis_connection, HashModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://127.0.0.1:8000'],
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+radis = get_redis_connection(
+    host="redis-12305.c305.ap-south-1-1.ec2.redns.redis-cloud.com",
+    port=12305,
+    password="uvxNKgVp1BbgGJSWgBZj38sMidb0YH7u",
+    decode_responses=True
+)
+
+class Product(HashModel):
+    name:str
+    price:float
+    quantity:int
+
+    class Meta:
+        database = radis
+
+@app.get('/products')
+def all():
+    return [format(pk) for pk in Product.all_pks()]
+
+def format(pk:str):
+        product = Product.get(pk)
+
+        return {
+            'id': product.pk,
+            'name':product.name,
+            'price':product.price,
+            'quantity':product.quantity
+        }
+
+
+@app.post('/products')
+def create(product:Product):
+    return product.save()
